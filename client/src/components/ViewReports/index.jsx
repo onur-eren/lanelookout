ac0553c8f190b61777a430adcd3d1888f44950d7
Manish Sinha <arithmetic@gmail.com>
2019-04-23 11:42:53 -0700

    hook up backend with leaflet map

--- client/src/components/ViewReports/index.jsx
+++ client/src/components/ViewReports/index.jsx
@@ -1,34 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import { DETAILS, coordsOakland } from "../../constants/report";
-import { Button, Container, Header } from 'semantic-ui-react'
+import { Button, Container, Form, Header } from 'semantic-ui-react'
 import HeatmapLayer from './HeatmapLayer';
 import { Link } from 'react-router-dom'
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
 
-export const points = [
-[37.81173258,-122.32513479],
-[37.76737559,-122.27947704],
-[37.77663943,-122.25980928],
-[37.80479155,-122.35611757],
-[37.75183947,-122.30445769],
-[37.83617435,-122.33878949],
-[37.74900933,-122.26927393],
-[37.79411245,-122.33279014],
-[37.80555317,-122.19062467],
-[37.87609362,-122.27589464],
-[37.87609363,-122.27589464],
-[37.87609365,-122.27589464],
-[37.87609361,-122.27589464],
-[37.87609362,-122.27589462]
-];
-
-
+const listReportsQuery = gql`
+  query {
+    listReports {
+        id
+        lat
+        lng
+    }
+  }
+`;
 
 const mapBottomOffset = 60;
 const initZoom = 13;
 
-const Heatmap = () => {
+const Heatmap = ({ data: { loading, listReports} }) => {
   // <Map> requires an absolute height
   const [height, setHeight] = useState(document.documentElement.clientHeight - mapBottomOffset);
   useEffect(() => {
@@ -54,28 +47,30 @@ const Heatmap = () => {
 
     return (
       <>
-      <Button fluid as={Link} to="/" size='massive'>Home</Button>
-      <Map
-        center={center}
-        zoom={zoom}
-        onDrag={onDrag}
-        onZoom={onZoom}
-        style={{ height }}
-      >
-          <HeatmapLayer
-            points={points}
-            longitudeExtractor={m => m[1]}
-            latitudeExtractor={m => m[0]}
-            intensityExtractor={m => parseFloat(m[2])}
-            radius={12}
-            />
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </Map>
+      <Form loading={loading}>
+          <Button fluid as={Link} to="/" size='massive'>Home</Button>
+        </Form>
+          <Map
+            center={center}
+            zoom={zoom}
+            onDrag={onDrag}
+            onZoom={onZoom}
+            style={{ height }}
+          >
+              <HeatmapLayer
+                points={listReports && listReports.map(report => ([report.lat, report.lng]))}
+                longitudeExtractor={m => m[1]}
+                latitudeExtractor={m => m[0]}
+                intensityExtractor={m => parseFloat(m[2])}
+                radius={12}
+                />
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+            </Map>
       </>
     );
 };
 
-export default Heatmap;
+export default graphql(listReportsQuery)(Heatmap)
