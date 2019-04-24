dbe7d63b4a98a01099962fe31c37ce39c5a8cd46
Baker Renneckar <bakerrenneckar@gmail.com>
2019-04-23 19:08:19 -0700

    Added copy to home page

--- client/src/components/PageHome/index.jsx
+++ client/src/components/PageHome/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import { put, call, select, takeLatest } from "redux-saga/effects";
-import { Button, Container, Header } from 'semantic-ui-react'
+import {  Button, Container, Header } from 'semantic-ui-react'
 
 const PageHome = () => (
   <Container className="main-container">
     <Header as='h1' textAlign="center">Lane Lookout</Header>
+    <Header as='h4' textAlign="center">An app to help Oakland cyclist report obstructions in biking infastructure.</Header>
+    <br/>
     <Button fluid as={Link} to="/report" size='massive'>Report Obstruction</Button>
     <br/>
     <Button fluid as={Link} to="/view_reports" size='massive'>View Reports</Button>
