502c8605a7bdc3e489a9919e5c909bab15da4dad
Manish Sinha <arithmetic@gmail.com>
2019-07-16 17:38:19 -0700

    fix url

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,47 @@
-import React from 'react';
-import { Router, Switch, Route } from "react-router-dom";
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import Home from "./views/Home";
-import Page404 from './views/Page404';
-import About from './views/About';
-import ReportObstruction from "./views/ReportObstruction";
+import React from 'react'
+import { Router, Switch, Route } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import Home from './views/Home'
+import Page404 from './views/Page404'
+import About from './views/About'
+import ReportObstruction from './views/ReportObstruction'
 import 'semantic-ui-css/semantic.min.css'
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga'
 import createHistory from 'history/createBrowserHistory'
 
-ReactGA.initialize("UA-140241188-1");
+ReactGA.initialize('UA-140241188-1')
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: process.env.NODE_ENV === "production" ? 'https://lanelookout.org/gql/' : "http://0.0.0.0:5000/gql/"}),
-    cache: new InMemoryCache(),
-          defaultOptions: {
+  link: new HttpLink({
+    uri:
+      process.env.NODE_ENV === 'production'
+        ? 'https://www.lanelookout.org/gql/'
+        : 'http://0.0.0.0:5000/gql/',
+  }),
+  cache: new InMemoryCache(),
+  defaultOptions: {
     watchQuery: {
       fetchPolicy: 'no-cache',
-      errorPolicy: 'ignore'
+      errorPolicy: 'ignore',
     },
     query: {
       fetchPolicy: 'no-cache',
-      errorPolicy: 'all'
+      errorPolicy: 'all',
     },
     mutate: {
-      errorPolicy: 'all'
-    }
-          }
-});
+      errorPolicy: 'all',
+    },
+  },
+})
 
 const history = createHistory()
 history.listen((location, action) => {
-  ReactGA.set({ page: location.pathname });
-  ReactGA.pageview(location.pathname);
-});
+  ReactGA.set({ page: location.pathname })
+  ReactGA.pageview(location.pathname)
+})
 
 const App = () => {
   return (
@@ -50,7 +55,7 @@ const App = () => {
         </Switch>
       </Router>
     </ApolloProvider>
-  );
+  )
 }
 
-export default App;
+export default App
